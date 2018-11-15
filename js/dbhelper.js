9c110c8489282e37472390e78c3b091d31dfe5b4
Madison Estabrook <madison.estabrook@gmail.com>
2018-11-15 08:17:14 -0500

    Added / in return value

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -10,7 +10,7 @@ class DBHelper {
   static get DATABASE_URL() {
     const port = 80 // Change this to your server port
      return `
-       http://localhost:${port}mws-restaurant-stage-1-ryan/data/restaurants.json
+       http://localhost:${port}/mws-restaurant-stage-1-ryan/data/restaurants.json
       `;
     // Changed 
   }
